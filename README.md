56f684d0f7097d23b392212f88e11b775493be4f
Guanyu Chen <501205587@qq.com>
2016-04-09 23:05:56 +0800

    解决了一些发牌的bug

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 #360前端星计划
+
+    // 自定义删除数组元素方法
+    // 在Array对象的原型上添加find方法，寻找指定元素在数组中的位置
+    Array.prototype.find = function(tem){
+        for(var i=0;i<this.length;i++){
+            if (this[i] == tem) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    // 在Array对象的原型上添加remove方法，按照find方法返回的索引，使用splice方法将指定元素删除
+    Array.prototype.remove = function(tem){
+        var i = this.find(tem);
+        if (i > -1) {
+            this.splice(i, 1);
+            return this[i];
+        }
+    }
+
+    delete tem[de]; // 利用delete操作符将发出的牌删除，把该位置的值赋为undefined，因为要保证数组长度不变
--- scripts/index.js
+++ scripts/index.js
@@ -4,16 +4,20 @@ window.onload = function(){
 		but = start.getElementsByTagName('button')[0],	// 开始游戏按钮
 		add = document.getElementById('add'),	// 下注按钮
 		addCard = document.getElementById('deal'),		// 发牌按钮
-		bet = document.getElementById('bet'),	//下注金额span
-		over = document.getElementById('over'),		//剩余金币span
+		bet = document.getElementById('bet'),	// 下注金额span
+		over = document.getElementById('over'),		// 剩余金币span
 		club = document.getElementsByClassName('club')[0],
 		diamond = document.getElementsByClassName('diamond')[0],
 		hearts = document.getElementsByClassName('hearts')[0],
-		spade = document.getElementsByClassName('spade')[0],
-		player = document.getElementById('players'),
-		maker = document.getElementById('makers'),
-		money = 0,		//奖池
-		count = [];		//52张牌加一个牌面
+		spade = document.getElementsByClassName('spade')[0],	// 四种花色的牌的ul
+		player = document.getElementById('players'),	// 玩家区域
+		maker = document.getElementById('makers'),		// 庄家区域
+		grade = document.getElementsByClassName('grade'),	// 点数
+		bef = document.getElementById('before'),	
+		aft = document.getElementById('after'),		// 中间信息区的两种页面
+		money = 0,		// 奖池
+		used = [],		// 每张牌是否用过
+		count = [];		// 52张牌加一个牌面
 
 	// 为扑克牌赋牌面
 	poker(club);
@@ -31,7 +35,12 @@ window.onload = function(){
 	}
 
 	addCard.onclick = function(){
-		deal();
+		deal(count);
+		bef.style.display = 'none';
+		aft.style.display = 'flex';
+		for(var i=0;i<grade.length;i++){
+			grade[i].parentNode.style.display = 'block';
+		}
 	}
 
 	// 下注
@@ -47,32 +56,39 @@ window.onload = function(){
 	}
 
 	// 开始发牌
-	function deal(){
-		var tem = count.concat();	//将扑克牌数组复制
+	function deal(tem){
 		if (parseInt(bet.innerHTML,10) > 0) {	// 每一次下注至少为5
 			money = parseInt(bet.innerHTML,10);		//赌注入奖池
 			bet.innerHTML = '0';	// 发牌后赌注归零
-			console.log('发牌啦');
-			// console.log(count);
-			every(tem,maker);
+			used[every(maker,0)] = 2;
+			used[every(player,1)] = 2;
+			console.log(used);
 		}
-	}
 
-	// 每次发牌
-	function every(tem,obj){
-		setTimeout(function(){
-			var ran = Math.round(Math.random()*tem.length),
-				ul = obj.getElementsByTagName('ul')[0];
-			if (ran < 12) {
-				ul.className = 'club';	
-			} else if (ran < 25) {
-				ul.className = 'diamond';
-			} else if (ran < 38) {
-				ul.className = 'spade';
-			}
-			tem[ran].style.display = 'block';
-			ul.appendChild(tem[ran]);
-		},500);
+		// 每次发牌
+		function every(obj,index){
+			
+				var ran = Math.round(Math.random() * tem.length),	// 随机数
+					ul = obj.getElementsByTagName('ul')[0];
+				while (used[ran] == 0) {
+					ran = Math.round(Math.random() * tem.length);
+				}
+				if (ran < 12) {
+					ul.className = 'club';	
+				} else if (ran < 25) {
+					ul.className = 'diamond';
+				} else if (ran < 38) {
+					ul.className = 'hearts';
+				} else {
+					ul.className = 'spade';
+				}
+				tem[ran].style.display = 'block';
+				ul.appendChild(tem[ran]);
+				console.log(ran);
+				grade[index].innerHTML = parseInt(grade[index].innerHTML) + parseInt(tem[ran].childNodes[0].innerHTML);
+				return ran;	// 返回本次发出的牌的索引
+			
+		}
 	}
 
 	// 通过js修改background-position值
@@ -89,6 +105,7 @@ window.onload = function(){
 				tem[i].style.backgroundPosition = -282+'px 90px';
 			}
 			count.push(tem[i]);
+			used.push(1);	// 每张牌没用过为1 用过为0
 		}
 	}
 }
